46857fd833a7f12da12374908cda6c57425d850d
Reagan Ongaya <reagan.ongaya@student.moringaschool.com>
2023-12-15 08:26:52 +0300

    Assignment

---
+++ app/migration/versions/e1796442fda9_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: e1796442fda9
+Revises: 
+Create Date: 2023-12-15 08:18:19.058314
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e1796442fda9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
